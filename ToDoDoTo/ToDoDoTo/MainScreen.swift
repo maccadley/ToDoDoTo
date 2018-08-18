3ff8aedd11e5120554df69fb965dba90ca7c077a
Admin <admin@mac-admin.local>
2018-08-18 12:47:45 +0300

    Added a simple functionality

--- ToDoDoTo/ToDoDoTo/MainScreen.swift
+++ ToDoDoTo/ToDoDoTo/MainScreen.swift
@@ -30,5 +30,20 @@ class MainScreen: UITableViewController {
         return itemArray.count
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    
 }
 
