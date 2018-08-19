5d98d3cf5c4528834ace5dcb898079ad098fd60c
Admin <admin@Mac-Admin.local>
2018-08-19 13:17:10 +0300

    Added Pod

--- ToDoDoTo/ToDoDoTo/Controller/CategoryTableView.swift
+++ ToDoDoTo/ToDoDoTo/Controller/CategoryTableView.swift
@@ -7,89 +7,86 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryTableView: UITableViewController {
 
+    var categoryArray = [Category]()
+    let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCategories()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    //MARK: - TableView DataSource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categoryArray.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category.name
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destinationVC = segue.destination as! MainScreen
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
     }
-    */
-
+    
+    //MARK: - DATA Manipulate data
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //Creating UIAlert action with TextField
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new ToDo Categories!", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            // what will happen once User hits add item
+            let newCategory = Category(context: self.contex)
+            newCategory.name = textField.text!
+            self.categoryArray.append(newCategory)
+            //Saving this Text to nameOfItem in ItemToDo class
+            self.saveDataAction()
+        }
+        alert.addTextField { (alertTexField) in
+            alertTexField.placeholder = "Create new item"
+            textField = alertTexField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Saving Data with core data
+    func saveDataAction(){
+        do{
+            try contex.save()
+        } catch{
+            print("Error saving \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    //We can use input parametr, or srick to the default, which is All the items insine this request
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+        do{
+            categoryArray = try contex.fetch(request)
+        } catch {
+            print("Something wrong: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    
+    
 }
--- ToDoDoTo/ToDoDoTo/Controller/MainScreen.swift
+++ ToDoDoTo/ToDoDoTo/Controller/MainScreen.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
-class MainScreen: UITableViewController {
+class MainScreen: UITableViewController, UISearchBarDelegate {
+    
     //Singleton of Item class.
-    var itemArray = [Items]()
+    var itemArray = [Item]()
+    //We selecting certain categiry and jump in it.
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
     //URL to Docements directory, and creating new file which will contains new Arrays of objects
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadItems()
         
     }
 
@@ -39,6 +46,8 @@ class MainScreen: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Switching BOOlean in Items and imediatly reloads the tableView
         itemArray[indexPath.row].doneOrNot = !itemArray[indexPath.row].doneOrNot
+//        contex.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
         saveDataAction()
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -49,8 +58,10 @@ class MainScreen: UITableViewController {
         let alert = UIAlertController(title: "Add new ToDo!", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once User hits add item
-            let newItem = Items()
+            let newItem = Item(context: self.contex)
             newItem.itemName = textField.text!
+            newItem.doneOrNot = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             //Saving this Text to nameOfItem in ItemToDo class
             self.saveDataAction()
@@ -62,29 +73,49 @@ class MainScreen: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    //MARK: - Decode and Encode the data.
-    // Need to use Encodable, Decodable or both - Codable protocol in certain class
+    //MARK: - Saving Data with core data
     func saveDataAction(){
-        let encoder = PropertyListEncoder()
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+          try contex.save()
         } catch{
-            print("error encondoing: \(error)")
+          print("Error saving \(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-            itemArray = try decoder.decode([Items].self, from: data)
-            } catch {
-                print("Error: \(error)")
+    //We can use input parametr, or srick to the default, which is All the items insine this request
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        let categorypredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categorypredicate, additionalPredicate])
+        } else {
+            request.predicate = categorypredicate
+        }
+        do{
+        itemArray = try contex.fetch(request)
+        } catch {
+            print("Something wrong: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "itemName CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "itemName", ascending: true)]
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
         }
     }
+    
+    
     
     
 }
